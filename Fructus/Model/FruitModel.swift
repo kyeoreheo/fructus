b04ccc441b80cf249b13339f0aa53b5d5f5cd307
Kyeore Heo <91kyoheo@gmail.com>
2021-03-30 19:54:25 -0400

    implemented model

--- Fructus/Model/FruitModel.swift
+++ Fructus/Model/FruitModel.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 // MARK:- Fruit Data Model
-//struct Fruit: Identifiable {
-//
-//}
+struct Fruit: Identifiable {
+    var id = UUID()
+    var title: String
+    var headline: String
+    var image: String
+    var gradientColors: [Color]
+    var description: String
+    var nutrition: [String]
+}
